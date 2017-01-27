f2c5d978de9f45c44efde0d43502abd3374c7ab9
Tony <burkun.anton@gmail.com>
2017-01-27 12:18:23 +0200

    test commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 //= ../../bower_components/jquery/dist/jquery.js
 //= ../../bower_components/waypoints/lib/jquery.waypoints.min.js
+//= ../../bower_components/gmaps/gmaps.js
 
 //= partials/app.js
--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -78,9 +78,12 @@ $(document).ready(function () {
         }
 
 
-
-
-
+//Maps
+    new GMaps({
+        div: '.map',
+        lat: -12.043333,
+        lng: -77.028333
+    });
 
     });
 
